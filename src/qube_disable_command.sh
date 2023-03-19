6c61e12da281eabef53f7be33142406d7ccead8b
wizardofhoms <wizardofhoms@proton.me>
2023-03-19 23:17:19 +0100

    Fixes

--- src/qube_disable_command.sh
+++ src/qube_disable_command.sh
@@ -5,6 +5,6 @@ vm="${args['vm']}"
 identity.set
 
 # Check VM ownership
-[[ "$(qube.owner "$vm")" != "$IDENTITY" ]] || _failure "VM $vm does not belong to $IDENTITY"
+[[ "$(qube.owner "$vm")" != "$IDENTITY\n" ]] || _failure "VM $vm does not belong to $IDENTITY"
 
 qube.disable "$vm"

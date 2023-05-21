e486966ae907fe33f32fb1d948c9f22d042e7534
wizardofhoms <wizardofhoms@proton.me>
2023-05-21 07:49:23 +0200

    Fixes

--- src/url_bookmark_command.sh
+++ src/url_bookmark_command.sh
@@ -12,7 +12,7 @@ split_vm="$(config_get SPLIT_BROWSER)"
 
 # Get the bookmark entry from either split-browser file, args, or user-input in prompt.
 if [[ -z "${url}" ]]; then
-    if web.bookmark.file_is_empty; then
+    # if web.bookmark.file_is_empty; then
         _info "No bookmarks file in ${split_vm}, prompting user to enter it."
         result="$(web.bookmark.prompt_create)"
         url="$( echo "${result}" | cut -f 1 -d $'\t')"
@@ -22,7 +22,7 @@ if [[ -z "${url}" ]]; then
     #     bookmark_entry="$(web.bookmark.prompt_pop)"
     #     url="$( echo "${bookmark_entry}" | awk '{print $2}' )"
     #     title="$( echo "${bookmark_entry}" | awk '{print $3}' )"
-    fi
+    # fi
 fi
 
 # We either have an entry, or some information to build one, otherwise abort.
